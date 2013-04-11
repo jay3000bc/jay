d585feb00f38f85b65d3a7f2bec7c8c24a533f3d
jay <jay3000bc@gmail.com>
2013-04-11 20:54:49 +0530

    Signup 1.1

---
+++ signupsuccess.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Revoluza</title>
+<link rel="icon" type="images/png" href="images/favicon.png" />
+<style type="text/css">
+@import url('css/registration.css');
+</style>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/Myriad_Pro_400.font.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/jquery.fancybox-1.3.4.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+</head>
+<body>
+    <div class="container">
+	<div class="header">
+		<div class="logo"></div>
+		<div class="header_middle"></div>
+		<div class="header_right">
+			<div class="top">
+				<div class="signin"><a href="#"></a></div>
+				<div class="newuser"><a href="#"></a></div>
+			</div>
+			<div class="bottom">
+				<div class="heading_name">
+					<span>You Need Help?</span>
+					<strong>Peter Smith</strong>
+				</div>
+				<div class="phone_number">
+					<span></span>
+					<small>555-555-555</small>
+				</div>
+				<div class="email_address">
+					<span></span>
+					<small>example@GGG.com</small>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Begin Horizontal Navigation -->
+	<div class="hor_nav">
+		<ul id="navigation">
+			<li class=""><a title='Home' href="index.html" class="active">Home</a></li>
+			<li class=""><a title='Porn_Industry' href="porn_industry.html">Porn Industry</a></li>
+			<li class=""><a title='Tube_Configurator' href="tube_configurator.html">Tube Configurator</a></li>
+		</ul>
+	</div>
+        <div class="content">
+            <h1>SUCCESS</h1>
+        </div>
+</div>
